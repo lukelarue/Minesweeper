5cc4f0780d2ba3736642af69233336214e11ac04
Luke LaRue <larvelarue@gmail.com>
2023-06-26 12:51:20 -0500

    Heroku app deploy v0.1
    uuid added to keep track of session so the game state doesn't change when used online

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session
 from MinesweeperEnv import Minesweeper
+import redis 
+import pickle 
+import uuid
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# connect to Redis - Local
+#r = redis.Redis(host='localhost', port=6379, db=0)
+
+# connect to Redis - Heroku
+r = redis.from_url(os.getenv("REDIS_URL"))
 
 @app.route('/')
 def home():
+    # Assign a UUID for each session if it doesn't exist
+    if 'uuid' not in session:
+        session['uuid'] = str(uuid.uuid4())
     return render_template('index.html')
 
 @app.route('/start', methods=['POST'])
@@ -18,8 +32,11 @@ def start():
 
     global env
     env = Minesweeper(n, m)
-
     _ = env.reset()
+
+    # pickle the environment and store in Redis
+    pickled_env = pickle.dumps(env)
+    r.set('env_' + session['uuid'], pickled_env)
 
     obs_list = env.game_state.tolist()
 
@@ -30,7 +47,17 @@ def move():
     data = request.get_json()
     action = int(data['action'])
 
+    # retrieve and unpickle the environment from Redis
+    pickled_env = r.get('env_' + session['uuid'])
+    if pickled_env is None:
+        return "Game not started. Please start the game first", 400
+    env = pickle.loads(pickled_env)
+
     obs, reward, terminated, truncated, info = env.step(action)
+
+    # pickle the environment and store back in Redis
+    pickled_env = pickle.dumps(env)
+    r.set('env_' + session['uuid'], pickled_env)
 
     # Assuming the observation is a 2D numpy array
     obs_list = env.game_state.tolist()
